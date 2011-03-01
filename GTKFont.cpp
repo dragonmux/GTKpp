250ef0d35474b44750655e308e6cffb345f5c910
Rachel Mant <dx-mon@users.sourceforge.net>
2011-03-01 16:57:20 +0000

    UPDATED: Reverted a patch applied to non-Windows OSes which was causing positining problems with the new X11 OpenGL font loader

--- GTKFont.cpp
+++ GTKFont.cpp
@@ -26,13 +26,10 @@ SIZE GTKFont::GetStringMetrics(const char *String)
 
 #ifdef _WINDOWS
 	pango_font_description_set_size(PFD, (FontSize - 1) * PANGO_SCALE);
-#endif
 	pango_layout_set_font_description(PL, PFD);
+#endif
 	pango_layout_get_pixel_extents(PL, NULL, &PR_Logic);
 	ret.cy = PR_Logic.height;
-#ifndef _WINDOWS	
-	ret.cy -= 10;
-#endif
 	ret.cx = PR_Logic.width;
 
 	g_object_unref(PL);
